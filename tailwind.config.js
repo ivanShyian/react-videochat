b04e3e6fe2a340e216c6046e95f1db1776cb8e2d
vanjkes <shyian.ivan@yahoo.com>
2022-07-20 15:12:44 +0300

    Configured config for displaying css images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {path.PlatformPath | path} */
+
+const path = require('path')
 
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'background-login': "url('src/assets/images/laptop.webp')"
+        'background-login': `url(${path.join(__dirname, 'src', 'assets/images', 'laptop.webp')})`
       }
     }
   },
-  plugins: [],
+  plugins: []
 }
